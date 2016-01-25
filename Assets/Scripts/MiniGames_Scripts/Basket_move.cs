1221283fdcaf5cf1835859612810cb5d25ba027e
Elio de Berardinis <eliodeb@gmail.com>
2016-01-25 14:15:17 +0000

    adding assets

---
+++ Assets/Scripts/MiniGames_Scripts/Basket_move.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basket_move : MonoBehaviour {
+
+    float MoveSpeed = 0.2f; //If you set it to public it will show in the unity GUI
+    
+
+
+    // Use this for initialization
+    void Start () {
+
+        this.transform.Translate(0, 0, 0);
+
+        
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        HandleMovement();
+
+
+    }
+
+
+    void HandleMovement()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(MoveSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(-MoveSpeed, 0, 0);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+
+        if (col.gameObject.tag=="Cherry") {
+
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MiniGames_Scripts/Fruit_Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fruit_Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //this.transform.Translate(0, -0.03f, 0);
+
+    }
+}
