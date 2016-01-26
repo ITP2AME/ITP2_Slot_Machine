6806ae72e8e73e92cf49a05194a98cd1a80a7361
Elio de Berardinis <eliodeb@gmail.com>
2016-01-26 18:32:16 +0000

    added more assets and scripts

---
+++ Assets/Scripts/MiniGames_Scripts/Spikes_Behaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikes_Behaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "Fruit")
+        {
+
+            Destroy(col.gameObject);
+
+        }
+    }
+}
