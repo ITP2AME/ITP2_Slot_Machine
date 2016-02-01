5f08b7e612679aa788e5544fd39de35ff8e71351
Elio de Berardinis <eliodeb@gmail.com>
2016-02-01 19:51:46 +0000

    minigame 2 update

---
+++ Assets/PreFabs/PrefabMinigame2/Fruit_Move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fruit_Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (Input.GetKey(KeyCode.Mouse0))
+        //{
+        //    this.transform.Translate(0.1f, 0.1f, 0);
+        //}
+
+    }
+
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+
+        this.transform.Translate(1.0f, 1.0f, 0);
+        //Destroy(this.gameObject);
+    }
+}
